e7776795cea075d2623bbe59be4d189c24b20f5a
michael pazinets <michael.pazinets@gmail.com>
2022-04-24 21:45:03 -0400

    button made

--- src/component/Input.jsx
+++ src/component/Input.jsx
@@ -1,7 +1,17 @@
 import { useState, useEffect } from "react";
 import { axios } from "axios";
 
+const Input = () => {
+    const [input, setInput] = useState(null)
 
+    
+    return(
+        <div>
+        <button>Find Your New Word</button>
+        </div> 
+    ) 
+
+}
 
 
 
