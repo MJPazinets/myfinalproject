ff99f7b66916a601605ef6f6c4c7026fc68ca06f
michael pazinets <michael.pazinets@gmail.com>
2022-04-25 20:06:41 -0400

    buttons

--- src/component/Input.jsx
+++ src/component/Input.jsx
@@ -4,7 +4,10 @@ import axios from "axios";
 const Input = () => {
     const [input, setInput] = useState("")
     const [results, setResults] = useState([])        
-    
+    const clear = () => {
+        setResults([])
+    }
+
     const submitThes = async () => {
         const url=`https://dictionaryapi.com/api/v3/references/thesaurus/json/${input}?key=8e7df91a-7eb0-4a27-88dc-a1403cd9fa2c`
         const res = await axios.get(url)
@@ -18,6 +21,8 @@ const Input = () => {
         <div>
         <input value={input} onChange={(event) => setInput(event.target.value)}  />
         <button onClick={submitThes}>Find Your New Word</button>
+        <button onClick={clear}>Clean this up!</button>
+
         <ul>
             {results.map(syn => {
                 return(
